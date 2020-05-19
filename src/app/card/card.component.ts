0f538df7a609e217f8881e18d8dddb20e7877517
Ludvig <luddenr1@live.se>
2020-05-19 11:05:28 +0200

    Some small stuff is still broke but added proper buttonl inks

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -1,21 +1,20 @@
 import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
-  selector: 'app-card',
-  templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+	selector: 'app-card',
+	templateUrl: './card.component.html',
+	styleUrls: [ './card.component.scss' ]
 })
-
 export class CardComponent implements OnInit {
+	@Input() cardTitle: String;
+	@Input() cardSubtitle: String;
+	@Input() cardImage: String;
+	@Input() cardContent: String;
+	@Input() githubButtonText: String;
+	@Input() githubButtonLink: String;
+	@Input() websiteButtonText: String;
+	@Input() websiteButtonLink: String;
+	constructor() {}
 
-  @Input() cardTitle: String;
-  @Input() cardSubtitle: String;
-  @Input() cardImage: String;
-  @Input() cardContent: String;
-  @Input() cardFooter: String;
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+	ngOnInit(): void {}
 }
